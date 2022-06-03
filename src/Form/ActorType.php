5d8b6c0c42efa062b49868b223b6a51161c39fed
Celie Russier <celie.russier@gmail.com>
2022-06-03 14:36:22 +0200

    quete 17, relation ManyToMany réciprocité infos à l'ajout d'un nouveau progrm au moment de l'enregistrement en BDD

--- src/Form/ActorType.php
+++ src/Form/ActorType.php
@@ -10,18 +10,16 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class ProgramType extends AbstractType
+class ActorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('synopsis', TextType::class)
-            ->add('poster', TextType::class)
-            ->add('category', null, ['choice_label' => 'name'])
-            ->add('actors', EntityType::class, [
-                'class' => Actor::class,
+            ->add('name', TextType::class)
+            ->add('programs', EntityType::class, [
+                'class' => Program::class,
                 'choice_label' => 'name',
+                'label' => 'Série',
                 'multiple' => true,
                 'expanded' => true,
                 'by_reference' => false,
